0c0aed51ef3f79feff49af61c386a0f8274a7f6b
LeviUSP <levifernandes@usp.br>
2020-10-02 12:08:17 -0300

    buying by whatsapp first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let quantity = 0;
+let quantitySpan = document.getElementById("quantity");
+let whatsappNumber = "+5515981064484";
+
+function addQuantity() {
+  quantity += 10;
+  quantitySpan.innerText = quantity;
+}
+
+function removeQuantity() {
+  if (quantity >= 10) {
+    quantity -= 10;
+    quantitySpan.innerText = quantity;
+  }
+}
+
+function buy() {
+  const whatsappMessage = `https://wa.me/${whatsappNumber}?text=Olá eu gostaria de ${quantity} cápsulas!`;
+  window.open(encodeURI(whatsappMessage));
+}
