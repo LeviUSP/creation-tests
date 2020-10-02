d5a96434ed15a8ce8a27b9b07d387666f0829f61
LeviUSP <levifernandes@usp.br>
2020-10-02 19:00:23 -0300

    a

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 let quantity = 0;
-let quantitySpan = document.getElementById("quantity");
+let quantityInput = document.getElementById("quantity");
 let whatsappNumber = "+5515981064484";
 
 function addQuantity() {
   quantity += 10;
-  quantitySpan.innerText = quantity;
+  quantityInput.value = quantity;
 }
 
 function removeQuantity() {
   if (quantity >= 10) {
     quantity -= 10;
-    quantitySpan.innerText = quantity;
+    quantityInput.value = quantity;
   }
 }
 
-function buy() {
+const phoneNumberInput = document.querySelector("input#phone-number");
+
+document.querySelector(".buy").addEventListener("click", (event) => {
+  event.preventDefault();
+  const phoneNumber = phoneNumberInput.value;
   if (quantity === 0) return;
-  const whatsappMessage = `https://wa.me/${whatsappNumber}?text=Hello, I would like ${quantity} from Italcarb 'K'!`;
+  const whatsappMessage = `https://wa.me/${whatsappNumber}?text=${getMessage(
+    quantity,
+    phoneNumber
+  )}`;
   window.open(encodeURI(whatsappMessage));
+});
+
+function getMessage(quantity, phoneNumber) {
+  return `
+    Date: ${new Date()}
+        Italcarb 'K':
+            Quantity: *${quantity}*
+            Additional Phone Number: *${phoneNumber}*
+    `;
 }
